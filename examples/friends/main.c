9f8106f9de5cf6dc5f92e722f99fa44c9c601c77
Krzesimir Nowak <krzesimir@kinvolk.io>
2016-09-27 15:19:14 +0200

    extend an example to download friend pictures

--- examples/friends/main.c
+++ examples/friends/main.c
@@ -4,6 +4,7 @@
 #include <goa/goa.h>
 #include <gfbgraph/gfbgraph.h>
 #include <gfbgraph/gfbgraph-goa-authorizer.h>
+#include <gdk-pixbuf/gdk-pixbuf.h>
 
 #undef GOA_API_IS_SUBJECT_TO_CHANGE
 
@@ -14,6 +15,43 @@ is_facebook_object (GoaObject *object)
   const gchar *provider_type = goa_account_get_provider_type (account);
 
   return g_strcmp0 (provider_type, "facebook") == 0;
+}
+
+static gboolean
+save_as_png (const gchar *filename,
+	     GBytes *raw,
+	     GError **error)
+{
+  GdkPixbufLoader *loader = gdk_pixbuf_loader_new ();
+  GdkPixbuf *pixbuf;
+  gboolean success = FALSE;
+
+  if (!gdk_pixbuf_loader_write_bytes (loader, raw, error))
+    {
+      g_prefix_error (error, "failed to write bytes to pixbuf loader: ");
+      goto out;
+    }
+  if (!gdk_pixbuf_loader_close (loader, error))
+    {
+      g_prefix_error (error, "failed to close a pixbuf loader: ");
+      goto out;
+    }
+  pixbuf = gdk_pixbuf_loader_get_pixbuf (loader);
+  if (pixbuf == NULL)
+    {
+      g_set_error_literal (error, G_IO_ERROR, G_IO_ERROR_FAILED, "no pixbuf?");
+      goto out;
+    }
+  if (!gdk_pixbuf_save (pixbuf, filename, "png", error, NULL))
+    {
+      g_prefix_error (error, "failed to save pixbuf as png: ");
+      goto out;
+    }
+  success = TRUE;
+ out:
+  if (loader != NULL)
+    g_object_unref (loader);
+  return success;
 }
 
 static gboolean
@@ -53,8 +91,35 @@ print_user_friends (GoaObject *facebook_account_object,
   for (iter = friends; iter != NULL; iter = iter->next)
     {
       GFBGraphUser *friend = GFBGRAPH_USER (iter->data);
+      GBytes *raw = NULL;
+      const gchar *friend_id = gfbgraph_node_get_id (GFBGRAPH_NODE (friend));
+      const gchar *name = gfbgraph_user_get_name (friend);
+      gchar *filename = NULL;
+      gboolean ok = FALSE;
 
-      g_print ("  - %s\n", gfbgraph_user_get_name (friend));
+      g_print ("  - %s\n", name);
+      raw = gfbgraph_user_get_picture (friend, auth, GFBGRAPH_PICTURE_LARGE, error);
+      if (raw == NULL)
+	{
+	  g_prefix_error (error, "failed to get picture of a friend with name %s: ", name);
+	  goto loop_out;
+	}
+      filename = g_strdup_printf("%s.png", friend_id);
+      if (!save_as_png (filename, raw, error))
+	{
+	  g_prefix_error (error, "failed to save picture of a friend with name %s to png file %s: ", name, filename);
+	  goto loop_out;
+	}
+      g_print ("    (PNG photo saved as %s)\n", filename);
+      ok = TRUE;
+
+    loop_out:
+      if (raw != NULL)
+	g_bytes_unref (raw);
+      if (filename != NULL)
+	g_free (filename);
+      if (!ok)
+	goto out;
     }
 
   success = TRUE;
